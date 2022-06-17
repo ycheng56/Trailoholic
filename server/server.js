b700d108672434285c86927c5b209ea7a25ccc50
ycheng56 <cynthiacheng95@gmail.com>
2022-06-16 22:03:59 -0700

    add Procfile

--- server/server.js
+++ server/server.js
@@ -5,19 +5,7 @@ const path = require("path");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 
-// app.use(express.static(path.join(__dirname, '/public/build')));
-
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('/public/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
-  });
-}
-
+app.use(express.static(path.join(__dirname, '/public/build')));
 
 
 app.use(cors());
