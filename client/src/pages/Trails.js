b3a291cf153e069c1154882de0569f96a121e4b7
SkylarZhuo <zhuohangli98@gmail.com>
2022-06-17 18:34:12 -0700

    add filter buttons in explore page, but unfinished the real function

--- client/src/pages/Trails.js
+++ client/src/pages/Trails.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import BikingFilter from '../components/FilterComponents/BikingFilter'
+import HikingFilter from '../components/FilterComponents/HikingFilter'
 import SearchTrails from '../components/SearchTrails'
 import TrailCards from '../components/TrailCards'
+import AllFilter from '../components/FilterComponents/AllFilter'
+import "./css/Trails.css"
 
 export default function Trails() {
   return (
     <div>
         <p>Explore All the Trails</p>
         <SearchTrails/>
+
+        <div className="filter-button"><AllFilter/></div>
+        <div className="filter-button"><HikingFilter/></div>
+        <div className="filter-button"><BikingFilter/></div>
+
         <TrailCards />
     </div>
   )
