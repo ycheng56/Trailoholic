07176208a1aa24ed6f8c572b03c977e3d5addf58
ycheng56 <cynthiacheng95@gmail.com>
2022-06-22 16:43:02 -0700

    edit home style

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,49 +7,51 @@ import Greeting from "../components/Greeting";
 
 export default function Home() {
   return (
-    <div>
-      <Carousel fade>
-        <Carousel.Item>
+    <div className="landing-page">
+      <Carousel fade className="landing-page-carousel">
+        <Carousel.Item className="carousel-item">
           <img
-            className="d-block w-100"
+            className="d-block w-100 min-vh-90"
             src={process.env.PUBLIC_URL + "/images/home_bg2.jpg"}
             alt="Home slide"
           />
-          <Carousel.Caption>
-          <Greeting/>
+          <Carousel.Caption className="carousel-caption">
+            <Greeting />
             <SearchTrails />
             <p>There are no shortcuts to any place worth going.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+
+        <Carousel.Item className="carousel-item">
           <img
-            className="d-block w-100"
+            className="d-block w-100 min-vh-90"
             src={process.env.PUBLIC_URL + "/images/home_bg1.jpg"}
             alt="Second slide"
           />
 
           <Carousel.Caption>
+            <Greeting />
             <SearchTrails />
             <p>
               Look deep into nature and you will understand everything better.
             </p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+
+        <Carousel.Item className="carousel-item">
           <img
-            className="d-block w-100"
+            className="d-block w-100 min-vh-90"
             src={process.env.PUBLIC_URL + "/images/home_bg3.jpg"}
             alt="Third slide"
           />
 
           <Carousel.Caption>
+            <Greeting />
             <SearchTrails />
             <p>The best views come after the hardest climb</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-      
-      <Link to="trails">Explore all routes</Link>
     </div>
   );
 }
