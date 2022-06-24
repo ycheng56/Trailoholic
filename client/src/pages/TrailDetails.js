e31f93dee3def68aefbb69894d3550ce32d9509f
SkylarZhuo <zhuohangli98@gmail.com>
2022-06-24 02:25:41 -0700

    iteration3-add more contents in detail page--Jump to different part/layout/add nearby cards

--- client/src/pages/TrailDetails.js
+++ client/src/pages/TrailDetails.js
@@ -3,7 +3,9 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import "./css/TrailDetails.css";
 import { useEffect, useState } from "react";
+import { FaDirections, FaHiking, FaRoad, FaRegClock } from "react-icons/fa";
 import MapSinglePoint from "../mapbox/MapSinglePoint";
+import PopularHikingTrail from "../components/TrailCollectionComponents/PopularHikingTrail"
 
 export default function TrailDetails() {
   const { user, isAuthenticated } = useAuth0();
@@ -32,7 +34,6 @@ export default function TrailDetails() {
     }
     fetchTrails();
   }, [trailId]);
-
 
   useEffect(() => {
     async function fetchUserList() {
@@ -99,8 +100,8 @@ export default function TrailDetails() {
   console.log(trails);
   // console.log("trails:",trails?.start["text_en"]);
   return (
-    <div className="trailDetail">
-      <div className="detailImg">
+    <div className="detail-wrapper">
+      {/* <div className="detailImg">
         <img
           alt="trail detail picture"
           src={process.env.PUBLIC_URL + "/images/trail_1.jpg"}
@@ -128,6 +129,69 @@ export default function TrailDetails() {
       )}
       <div className="map-container">
         <MapSinglePoint trails={trails} Lng={Lng} Lat={Lat} />
+      </div> */}
+
+      <div className="detail-img">
+        <img
+          alt="trail detail picture"
+          src={process.env.PUBLIC_URL + "/images/trail_1.jpg"}
+        ></img>
+      </div>
+      <div className="detail-navbar">
+        <a href="#detail-type">TRAIL DETAIL</a>
+        <a href="#detail-instruction">INSTRUCTION</a>
+        <a href="#detail-map">LOCATION</a>
+        <a href="#detail-nearby">WHAT'S NEARBY</a>
+      </div>
+
+      <div className="detail-type" id="detail-type">
+        <h1>TRAIL DETAIL</h1>
+        <div className="detail-type-wrapper">
+          <div>
+            <FaHiking size="2rem" />
+            <p>Trail Type</p>
+            <p>{trails.mode}</p>
+          </div>
+          <div>
+            <FaRoad size="2rem" />
+            <p>Trail Distance</p>
+            <p>{trails.distance} km</p>
+          </div>
+          <div>
+            <FaRegClock size="2rem" />
+            <p>Trail Duration</p>
+            <p>{trails.duration} mins</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="detail-instruction" id="detail-instruction">
+        <h1>INSTRUCTION</h1>
+        <p>Starts from:{trails.start?.["text_en"]}</p>
+        <p>Destination:{trails.destination?.["text_en"]}</p>
+        <p>
+          Instruction for the trail:
+          {instruction.map((item, index) => (
+            <li key={index}>
+              <FaDirections />
+              {item}
+            </li>
+          ))}
+        </p>
+      </div>
+
+      <div className="detail-map" id="detail-map">
+      <h1>Location</h1>
+        <div className="map-container">
+          <MapSinglePoint trails={trails} Lng={Lng} Lat={Lat} />
+        </div>
+      </div>
+
+      <div className="detail-nearby" id="detail-nearby">
+        <h1>What's Nearby</h1>
+        <div className="detail-nearby-card">
+          <PopularHikingTrail/><PopularHikingTrail/><PopularHikingTrail/><PopularHikingTrail/>
+        </div>
       </div>
     </div>
   );
