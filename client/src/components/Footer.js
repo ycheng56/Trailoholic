288ada618631df938959fcb2fb5774819334d13d
SkylarZhuo <zhuohangli98@gmail.com>
2022-06-24 01:17:12 -0700

    update footer layout

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,19 +3,21 @@ import { Container, Row, Col } from "react-bootstrap";
 import "./css/Navbar.css";
 export default function Footer() {
   return (
-    <footer className="bg-light p-3 text-center">
+    <footer className="bg-light p-1 text-center">
       <Container>
-        <Row>
-          <Col>
-          <img src={process.env.PUBLIC_URL + "/images/logo_3.png"} alt="logo" className="banner-logo"></img>
-          </Col>
-        </Row>
-        <Row>
+      <Row>
+        <Col>
+        <img src={process.env.PUBLIC_URL + "/images/logo_3.png"} alt="logo" className="banner-logo"></img>
+        </Col>
+        <Col className="footer-wrapper">
+        <Row className="footer-row">
           <Col>Copyright 2022 Trailaholic Brands, Inc.</Col>
         </Row>
         <Row>
           <Col>Privacy Policy Use of Cookies Legal</Col>
         </Row>
+        </Col>
+      </Row> 
       </Container>
     </footer>
   );
